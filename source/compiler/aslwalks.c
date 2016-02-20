a43afff03ab1bfa73edc78728bfd30fc86e96d72
RehabMan <racerrehabman@gmail.com>
2016-02-20 07:59:43 -0800

    make 'result not used' (3114) warning instead of error

--- source/compiler/aslwalks.c
+++ source/compiler/aslwalks.c
@@ -661,14 +661,14 @@ AnOtherSemanticAnalysisWalkBegin (
                     (PrevArgOp) &&
                     (PrevArgOp->Asl.ParseOpcode == PARSEOP_ZERO))
                 {
-                    AslError (ASL_ERROR, ASL_MSG_RESULT_NOT_USED,
+                    AslError (ASL_WARNING/*ASL_ERROR*/, ASL_MSG_RESULT_NOT_USED,
                         Op, Op->Asl.ExternalName);
                 }
             }
 
             else if (ArgOp->Asl.ParseOpcode == PARSEOP_ZERO)
             {
-                AslError (ASL_ERROR, ASL_MSG_RESULT_NOT_USED,
+                AslError (ASL_WARNING/*ASL_ERROR*/, ASL_MSG_RESULT_NOT_USED,
                     Op, Op->Asl.ExternalName);
             }
         }
@@ -688,7 +688,7 @@ AnOtherSemanticAnalysisWalkBegin (
 
             default:
 
-                AslError (ASL_ERROR, ASL_MSG_RESULT_NOT_USED,
+                AslError (ASL_WARNING/*ASL_ERROR*/, ASL_MSG_RESULT_NOT_USED,
                     Op, Op->Asl.ExternalName);
                 break;
             }
