bf0747996069c49afa4a6faf3fc6a89ea6d84044
RehabMan <racerrehabman@gmail.com>
2016-04-10 08:12:55 -0700

    bump version 20160410

--- source/include/acpixf.h
+++ source/include/acpixf.h
@@ -118,7 +118,7 @@
 
 /* Current ACPICA subsystem version in YYYYMMDD format */
 
-#define ACPI_CA_VERSION                 0x20160409
+#define ACPI_CA_VERSION                 0x20160410
 
 #include "acconfig.h"
 #include "actypes.h"
